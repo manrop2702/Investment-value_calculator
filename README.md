aff70edf0f99db45abedbe20fdbf91a1e201034a
manrop2702 <nminh8853@gmail.com>
2021-04-20 20:14:39 +0700

    First commit: Add README file and create project.

---
+++ README.md
@@ -0,0 +1,13 @@
+******* Bai tap Java GUI voi thu vien java swing *******
+
+Investment-value calculator
+
+					 === INPUT ===
+		-- Investment amout.
+		-- Number of years.
+		-- Annual interest rate.
+		-- Future value.
+					=== FORMULA ===
+		futureValue = investmentAmount * (1 + monthlyInterestRate)^(year*12)
+
+- FRAME LAYOUT: GridBag
---
+++ investment_value_calc.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.math.*;
+
+public class investment_value_calc {
+	JFrame frame = new JFrame("Investment-value Calculator");
+	JPanel panel = new JPanel();
+
+
+
+
+
+
+	public investment_value_calc() {
+		createFrame();
+		addComponents();
+		addEventListener();
+	}
+
+
+	public void createFrame() {
+		frame.setSize(400, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public void addComponents() {
+
+	}
+
+
+
+
+	public static void main(String[] args) {
+		
+	}
+}
